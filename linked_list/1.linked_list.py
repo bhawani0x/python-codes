fca7c126045935ff1072405c93af9d1a5159a508
Bhawani Singh <74950754+bhawani0x@users.noreply.github.com>
2025-07-20 00:19:35 +0530

    linked list

---
+++ linked_list/1.linked_list.py
@@ -0,0 +1,13 @@
+# lets create linked list 
+# linked list is basically a node that contain data and pointer 
+# create a linked list class and perform action like creating, inserting, append/add new node, pop/remove node 
+
+class linked_list():
+    def __init__(self, data):
+        self
+        self.data = data
+        self.next = None
+
+
+obj1 = linked_list(4)
+print(obj1)
